3f8e7bb976fe2d1bb264a1f8e9df7e5d482556b8
HAO CHU <chuhaono.1@hotmail.com>
2019-10-07 03:05:48 -0400

    test what does stage do

--- Python/starter.py
+++
@@ -1,9 +0,0 @@
-name = 'hao chu'
-country = 'Canada'
-age = 234
-hourly_wage = 234423234242
-satisfied = True
-daily_wage = hourly_wage*8
-
-print(name + country + str(age) + str(hourly_wage))
-print(f"hi, my name is {name} and i make {hourly_wage}")
